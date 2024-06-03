ed14d55a96beafc7bde0c13421768d0bea2f7b5f
maingoclong395 <maingoclong395@gmail.com>
2024-06-03 10:21:09 +0700

    feat(config): 'config theme color for source'

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "primary-color": "#23587B",
+      "secondary-color": "#57CC99",
+      "text-color": "#000",
+      "background-color": "#fff",
+      "accent-color": "#38A3A5",
+      "accent-color2": "#80ED99",
+      "accent-color3": "#C7F9CC",
+      "accent-color4": "#174444",
+      "static-color": "#F7F7F7",
+    },
   },
   plugins: [],
 };
