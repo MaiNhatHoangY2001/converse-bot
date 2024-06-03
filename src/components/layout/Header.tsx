22ff4fc697e5c35eec946e15df91501e8b3b40e3
maingoclong395 <maingoclong395@gmail.com>
2024-06-03 11:58:09 +0700

    feat: Add welcome page and header layout

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,63 @@
+import {useState} from "react";
+import ReactFlagsSelect from "react-flags-select";
+import IMAGES from "../../constants/imgUrl";
+import LinkBtn from "../common/LinkBtn";
+
+import HeaderStyle from "./Header.module.scss";
+
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(HeaderStyle);
+
+export default function Header() {
+  const [selectedCode, setSelectedCode] = useState("VN");
+
+  return (
+    <div
+      className={cx([
+        "container",
+        "w-full h-[4.25rem] grid grid-cols-10 items-center bg-accent-color text-[white]",
+      ])}>
+      <div className="col-span-2 flex justify-center items-center">
+        <img src={IMAGES.logo} alt="logo" width={54} height={50} />
+        <div className={cx(["converseBot", "font-bold text-[1.375rem]"])}>
+          ConverseBot
+        </div>
+      </div>
+      <div
+        className={cx([
+          "wrapperToolbar",
+          "col-span-8 flex justify-end gap-4 mr-[6%]",
+        ])}>
+        <ReactFlagsSelect
+          className="border-r-[1px] border-solid border-[white] pr-4"
+          countries={["VN", "US"]}
+          // customLabels={{ VN: 'Vietnam', US: 'English' }}
+          showSelectedLabel={false}
+          showOptionLabel={false}
+          fullWidth={false}
+          selectButtonClassName="border-none"
+          selectedSize={20}
+          selected={selectedCode}
+          onSelect={code => setSelectedCode(code)}
+        />
+        <LinkBtn
+          className={cx([
+            "loginBtn",
+            "bg-accent-color text-sm w-[10%] hover:bg-accent-color4",
+          ])}
+          to="/auth/login">
+          LOGIN
+        </LinkBtn>
+        <LinkBtn
+          className={cx([
+            "registerBtn",
+            "bg-primary-color text-sm w-[10%] hover:bg-accent-color4",
+          ])}
+          to="/auth/register">
+          REGISTER
+        </LinkBtn>
+      </div>
+    </div>
+  );
+}
--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,12 +1,14 @@
 import {Navigate, Route, Routes} from "react-router-dom";
 import Login from "../pages/auth/Login";
 import {Register} from "../pages/auth/Register";
+import Welcome from "../pages/auth/Welcome";
 
 export default function PublicLayout() {
   return (
     <div>
-      <h1>Public Pages</h1>
+      {/* <h1>Public Pages</h1> */}
       <Routes>
+        <Route path="welcome" element={<Welcome />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="*" element={<Navigate to="/auth/login" />} />
